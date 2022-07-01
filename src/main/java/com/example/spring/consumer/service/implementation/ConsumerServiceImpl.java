da17ecdea2af7bf19eed07aeb29fdae59cfee93a
demisgps <demisgps@gmail.com>
2022-07-01 10:22:27 -0300

    Adicionando configurações

---
+++ src/main/java/com/example/spring/consumer/service/implementation/ConsumerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.spring.consumer.service.implementation;
+
+import com.example.spring.consumer.dto.Message;
+import com.example.spring.consumer.service.ConsumerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerServiceImpl implements ConsumerService {
+    @Override
+    public void action(Message message) {
+        System.out.println(message.getText());
+    }
+}
